077abc3b51b99f2e9e9d3cd67e1455fed2575dd3
John Ferlito <johnf@inodes.org>
2023-07-05 21:37:18 +1000

    Add S3 adapter

---
+++ packages/proxyist-adapter-s3/src/index.ts
@@ -0,0 +1,103 @@
+import {
+  S3Client,
+  HeadObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+} from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
+
+import type { CreateAdapter, AdapterConfig } from 'proxyist-adapter-common';
+import stream, { Readable } from 'stream';
+
+interface S3AdapterConfig extends AdapterConfig {
+  bucket: string,
+  prefix?: string,
+  s3?: S3Client,
+  transform: (identifier: string) => string,
+}
+
+export const createAdapter: CreateAdapter<S3AdapterConfig> = (config) => {
+  const s3 = config.s3 || new S3Client({});
+  const bucketName = config.bucket;
+  const prefix = config.prefix || '';
+
+  const getPath = (identifier: string, filename: string) => {
+    const path = config.transform(identifier);
+
+    return `${prefix}/${path}/${filename}`;
+  };
+
+  const exists = async (identifier: string, filename: string) => {
+    const path = getPath(identifier, filename);
+
+    const command = new HeadObjectCommand({
+      Bucket: bucketName,
+      Key: path,
+    });
+
+    try {
+      await s3.send(command);
+
+      return true;
+    } catch (error) {
+      if (error instanceof Error && error.name === 'NotFound') {
+        return false;
+      }
+
+      throw error;
+    }
+  };
+
+  const read = async (identifier: string, filename: string) => {
+    const path = getPath(identifier, filename);
+
+    const command = new GetObjectCommand({
+      Bucket: bucketName,
+      Key: path,
+    });
+
+    const object = await s3.send(command);
+
+    return object.Body as Readable;
+  };
+
+  const write = async (identifier: string, filename: string) => {
+    const path = getPath(identifier, filename);
+
+    const ws = new stream.PassThrough();
+    console.debug('write', path);
+
+    const command = {
+      client: s3,
+      params: {
+        Bucket: bucketName,
+        Key: path,
+        Body: ws,
+      },
+    };
+
+    const upload = new Upload(command);
+
+    const promise = upload.done();
+
+    return { ws, promise };
+  };
+
+  const rm = async (identifier: string, filename: string) => {
+    const path = getPath(identifier, filename);
+
+    const command = new DeleteObjectCommand({
+      Bucket: bucketName,
+      Key: path,
+    });
+
+    await s3.send(command);
+  };
+
+  return {
+    exists,
+    read,
+    write,
+    rm,
+  };
+};
