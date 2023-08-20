5604f01f8cff9f4e2a2ccd0da642e46940de70f6
John Ferlito <johnf@inodes.org>
2023-08-20 15:04:23 +1000

    Fix route name

--- packages/proxyist/src/routes/index.ts
+++ packages/proxyist/src/routes/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Proxyist' });
 });
 
 export default router;
